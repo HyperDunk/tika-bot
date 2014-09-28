c43c19a87a2a96916778720f828aecff2dc5ff7a
HyperDunk <aakarsh.mhm@gmail.com>
2014-09-28 16:36:19 -0700

    removing output directory

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 tika-bot
 ========
+
+arguments to execute (all are must, no optional):
+
+Path to folder containing input TSV files: -i "path"
+Path to output folder where xhtml files should be written: -xo "path"
+Path to output folder where json files should be written: -jo "path" 
+
+Note: Make sure you have linked tika-app-1.6.jar library to your project / is part of the library.
--- src/edu/usc/csci572/crawler/TSVParser.java
+++ src/edu/usc/csci572/crawler/TSVParser.java
@@ -23,12 +23,12 @@ public class TSVParser extends AbstractParser {
 
 	private static final long serialVersionUID = -7703188620341736671L;
 
-	private static final Set<MediaType> SUPPORTED_TYPES =
-        Collections.singleton(MediaType.TEXT_HTML);
-	
-	private static final ServiceLoader LOADER =
-        new ServiceLoader(TSVParser.class.getClassLoader());
-	
+	private static final Set<MediaType> SUPPORTED_TYPES = Collections
+			.singleton(MediaType.TEXT_HTML);
+
+	private static final ServiceLoader LOADER = new ServiceLoader(
+			TSVParser.class.getClassLoader());
+
 	@Override
 	public Set<MediaType> getSupportedTypes(ParseContext arg0) {
 		// TODO Auto-generated method stub
@@ -36,56 +36,61 @@ public class TSVParser extends AbstractParser {
 	}
 
 	@Override
-	public void parse(InputStream stream, ContentHandler handler, Metadata metadata,
-			ParseContext context) throws IOException, SAXException, TikaException {
+	public void parse(InputStream stream, ContentHandler handler,
+			Metadata metadata, ParseContext context) throws IOException,
+			SAXException, TikaException {
 		// TODO Auto-generated method stub
-		BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-        try {
-            Charset charset = Charset.forName("UTF-8");
-            MediaType type = new MediaType(MediaType.parse("application/html+xml"), charset);
-            metadata.set(Metadata.CONTENT_TYPE, type.toString());
-            metadata.set(Metadata.CONTENT_ENCODING, charset.name());
+		BufferedReader reader = new BufferedReader(
+				new InputStreamReader(stream));
+		try {
+			Charset charset = Charset.forName("UTF-8");
+			MediaType type = new MediaType(
+					MediaType.parse("application/html+xml"), charset);
+			metadata.set(Metadata.CONTENT_TYPE, type.toString());
+			metadata.set(Metadata.CONTENT_ENCODING, charset.name());
 
-            XHTMLContentHandler xhtml =
-                    new XHTMLContentHandler(handler, metadata);
-            xhtml.startDocument();
-            
-            xhtml.startElement("table");
-            String line;
-            
-            String headers[] = {"postedDate", "location", "department", "title", "salary","dummy", "start", "duration", 
-            		"jobtype", "applications", "company", "contactPerson", "phoneNumber", "faxNumber", "location", 
-            		"latitude", "longitude", "firstSeenDate", "url", "lastSeenDate"};
-            xhtml.startElement("tr");
-            
-            PrintWriter out = new PrintWriter("count.txt", "UTF-8");
-            out.println("head: " + headers.length);
-            for(int i=0; i<headers.length; i++) {
-            	xhtml.startElement("th");
-            	xhtml.characters(headers[i]);
-            	xhtml.endElement("th");
-            }
-            xhtml.endElement("tr");
-            while((line = reader.readLine()) != null) {
-            	xhtml.startElement("tr");
-            	
-            	String[] columns = line.split("\t");
-            	out.println(columns.length);
-            	for(int i=0; i<columns.length; i++) {
-            		xhtml.startElement("td");
-            		xhtml.characters(columns[i]);
-            		xhtml.endElement("td");
-            	}
-            	
-            	xhtml.endElement("tr");
-            }
-            xhtml.endElement("table");
-            out.close();
-            xhtml.endDocument();
-        } finally {
-            reader.close();
-        }
-		
+			XHTMLContentHandler xhtml = new XHTMLContentHandler(handler,
+					metadata);
+			xhtml.startDocument();
+
+			xhtml.startElement("table");
+			String line;
+
+			String headers[] = { "postedDate", "location", "department",
+					"title", "salary", "dummy", "start", "duration", "jobtype",
+					"applications", "company", "contactPerson", "phoneNumber",
+					"faxNumber", "location", "latitude", "longitude",
+					"firstSeenDate", "url", "lastSeenDate" };
+			xhtml.startElement("tr");
+
+			PrintWriter out = new PrintWriter("count.txt", "UTF-8");
+			out.println("head: " + headers.length);
+			for (int i = 0; i < headers.length; i++) {
+				xhtml.startElement("th");
+				xhtml.characters(headers[i]);
+				xhtml.endElement("th");
+			}
+			xhtml.endElement("tr");
+			while ((line = reader.readLine()) != null) {
+				xhtml.startElement("tr");
+
+				String[] columns = line.split("\t");
+				out.println(columns.length);
+				for (int i = 0; i < columns.length; i++) {
+					xhtml.startElement("td");
+					xhtml.characters(columns[i]);
+					xhtml.endElement("td");
+				}
+
+				xhtml.endElement("tr");
+			}
+			xhtml.endElement("table");
+			out.close();
+			xhtml.endDocument();
+		} finally {
+			reader.close();
+		}
+
 	}
 
 }
